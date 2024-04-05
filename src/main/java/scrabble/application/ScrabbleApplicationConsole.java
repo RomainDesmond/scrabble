7fcc1940f7133e1254e277b2721743ccf45d6467
nchaussat1 <noa.chaussat@etu.unilim.fr>
2024-04-05 13:58:17 +0200

    Ajout ligne Noa

--- src/main/java/scrabble/application/ScrabbleApplicationConsole.java
+++ src/main/java/scrabble/application/ScrabbleApplicationConsole.java
@@ -5,6 +5,7 @@ public class ScrabbleApplicationConsole {
 	public static void main(String[] args) {
 		System.out.println("---------------------------------------------------------");
 		System.out.println("--  Bienvenue dans notre magnifique jeu de scrabble !  --");
+		System.out.println("--  developpé par Noa                                  --");
 		System.out.println("---------------------------------------------------------");
 	}
 
