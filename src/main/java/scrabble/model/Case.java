59f8d869012dba5fa02870d615ba8364a60cca1f
hmille <hippolyte.mille@etu.unilim.fr>
2024-04-09 18:53:40 +0200

    Ajout des variables, des constructeurs et des classes classes Jeu et Case

--- src/main/java/scrabble/model/Case.java
+++ src/main/java/scrabble/model/Case.java
@@ -1,5 +1,28 @@
 package scrabble.model;
 
 public class Case {
-	//TODO
+	private int posX;
+	private int posY;
+	private String typeCase;
+	private Lettre contenu;
+	
+	public Case(int posX, int posY, String typeCase, Lettre contenu) {
+		this.posX=posX;
+		this.posY=posY;
+		this.typeCase=typeCase;
+		this.contenu=contenu;		
+	}
+	
+	public Boolean estUtilise() {
+		//a compléter
+	}
+	
+	public int compterPoint() {
+		//a compléter
+	}
+	
+	public Boolean lettrePeutEtrePlace() {
+		//a compléter
+	}
+	
 }
--- src/main/java/scrabble/model/Jeu.java
+++ src/main/java/scrabble/model/Jeu.java
@@ -1,5 +1,47 @@
 package scrabble.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Jeu {
-	//TODO
+	private int tour;
+	private List<String> motsValides;
+	private List<Lettre> sacDeLettre;
+	private ArrayList<Case> plateau;
+	private List<Joueur> listeJoueur;
+	
+	plateau = new ArrayList<Case> (225);
+
+	public Jeu(int tour, List<String> motsValides, List<Lettre> sacDeLettre, ArrayList<Case> plateau, List<Joueur> listeJoueur) {
+		this.tour=tour;
+		this.motsValides=motsValides;
+		this.sacDeLettre=sacDeLettre;
+		this.plateau=plateau;
+		this.listeJoueur=listeJoueur;
+	}
+	
+	public Boolean verifierMot() {
+		//a compléter
+	}
+	
+	public int compterPoint() {
+		//a compléter
+	}
+	
+	public Lettre distribuerLettre() {
+		//a compléter
+	}
+	
+	public String joueurQuiJoue() {
+		//a compléter
+	}
+	
+	public List<List<Case>> afficherPlateau() {
+		//a compléter
+	}
+	
+	public String finPartie() {
+		//a compléter
+	}
+	
 }
