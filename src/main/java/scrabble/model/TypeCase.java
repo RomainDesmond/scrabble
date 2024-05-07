6e62203da330f246d811a9abf06fd81d7dc043f8
Romain DESMOND <romain.desmond@etu.unilim.fr>
2024-05-07 16:25:01 +0200

    Ajout de valeurs et d'un affichage pour l'énumération TypeCase

--- src/main/java/scrabble/model/TypeCase.java
+++ src/main/java/scrabble/model/TypeCase.java
@@ -1,10 +1,18 @@
 package scrabble.model;
 
 public enum TypeCase {
-	LETTREDOUBLE,
-	LETTRETRIPLE,
-	MOTDOUBLE,
-	MOTTRIPLE,
-	NORMAL,
-	DEPART,
+	LETTREDOUBLE("LD"),
+	LETTRETRIPLE("LT"),
+	MOTDOUBLE("MD"),
+	MOTTRIPLE("MT"),
+	VIDE(""),
+	DEPART("*");
+	private String affichageCase;
+	
+	private TypeCase(String affichageCase) {
+		this.affichageCase=affichageCase;
+	}
+	public String AffichageCase() {
+		return this.affichageCase;
+	}
 }
