fec172dd9da6bbb8f046a2ab40dc9830f1b27665
Romain DESMOND <romain.desmond@etu.unilim.fr>
2024-05-07 09:36:52 +0200

    Ajout du package scrabble.exception et de la classe ScrabbleException

---
+++ src/main/java/scrabble/exception/ScrabbleException.java
@@ -0,0 +1,15 @@
+package scrabble.exception;
+
+import java.io.IOException;
+
+import scrabble.model.Lettre;
+
+public class ScrabbleException {
+	public static void neDepassePasLindex (Lettre[][] listeLettre, int position1,int position2,Lettre lettre) {
+		//WIP
+
+			//return la liste de base si non possible
+		//return la nouvelle liste
+
+	}
+}
