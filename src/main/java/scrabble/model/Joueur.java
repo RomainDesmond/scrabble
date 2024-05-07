78472ca3ab4e4f7c02a25b1d086d562196e8315b
Romain DESMOND <romain.desmond@etu.unilim.fr>
2024-05-07 16:45:37 +0200

    Ajout d'un constructeur Joueur Ajout de la méthode afficherChevalet,ajout d'une méthode sacDeLettreEstVide Ajout de la méthode ajouterLettre finalisation de la méthode placeVideChevalet

--- src/main/java/scrabble/model/Joueur.java
+++ src/main/java/scrabble/model/Joueur.java
@@ -9,7 +9,9 @@ import scrabble.console.Console;
 public class Joueur {
 	private String nom;
 	private int score;
-	private List<Lettre> chevalet;
+	static Lettre [] chevalet;
+
+	//private List<Lettre> chevalet;
 	private int valeurEchange;
 	
 	
@@ -18,7 +20,25 @@ public class Joueur {
 	public Joueur(String nom, int score, List<Lettre> chevalet) {
 		this.nom=nom;
 		this.score=score;
-		this.chevalet=chevalet;
+		this.chevalet = new Lettre[7];
+	}
+	public Joueur(String nom) {
+		this.nom=nom;
+		this.score=0;
+		this.chevalet = new Lettre[7];
+	}
+	public void afficherChevalet() {
+		System.out.println("1 2 3 4 5 6 7");
+		for (int compteur=0;compteur<7;compteur++) {
+			if (chevalet[compteur]!=null) {
+				System.out.print(chevalet[compteur].AffichageLettre()+" ");
+		}
+			else {
+				System.out.print(chevalet[compteur]);
+
+			}
+	}
+	System.out.println();
 	}
 	
 	public void echangerLettre(int nombreAEchanger) {
@@ -27,6 +47,17 @@ public class Joueur {
 
 		
 	}
+	public boolean sacDeLettreEstVide() {
+		boolean booleenEstVide = true;
+		for (int cpt = 0; cpt<7; cpt++) {
+			if (!(chevalet[cpt]== null)) {
+				booleenEstVide=false;
+				break;		
+			}
+		}
+        return booleenEstVide;
+	}
+	
 
 	public void jouerLettre() {
 		//TODO
@@ -46,8 +77,26 @@ public class Joueur {
 	public void entrerNom() {
 		//TODO
 	}
-	public void placeVideChevalet(){
-		//TODO
+	public boolean ajouterLettre(Lettre lettre) {
+		int position=0;
+		boolean booleenAjoutLettre=false;
+		while (position<7){
+			if (placeVideChevalet(position)) {
+				chevalet[position]=lettre;
+				booleenAjoutLettre=true;
+				break;
+			}
+			position++;
+		}
+		return booleenAjoutLettre;
+
+	}
+	public boolean placeVideChevalet(int position){
+		boolean booleenEstVide = true;
+			if (!(chevalet[position]== null)) {
+				booleenEstVide=false;			}
+		
+        return booleenEstVide;
 	}
 	public void afficherJoueurEtScore(){
 		//TODO
@@ -67,14 +116,6 @@ public class Joueur {
 
 	public void setScore(int score) {
 		this.score = score;
-	}
-
-	public List<Lettre> getChevalet() {
-		return chevalet;
-	}
-
-	public void setChevalet(List<Lettre> chevalet) {
-		this.chevalet = chevalet;
 	}
 
 	public int getValeurEchange() {
