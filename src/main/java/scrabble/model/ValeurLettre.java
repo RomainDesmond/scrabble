09857b07261f3a94d583961acc11be5e3e039727
Romain DESMOND <romain.desmond@etu.unilim.fr>
2024-05-13 16:31:57 +0200

    Ajout getteur, setteur et changement en variable final de recurrence et point

--- src/main/java/scrabble/model/ValeurLettre.java
+++ src/main/java/scrabble/model/ValeurLettre.java
@@ -4,8 +4,8 @@ public enum ValeurLettre {
 	A ("A",9,1),B("B",2,3),C("C",2,3),D("D",3,2),E("E",15,1),F("F",2,4),G("G",2,2),H("H",2,4),I("I",8,1),J("J",1,8),K("K",1,10),L("L",5,1),M("M",3,2),N("N",6,1),O("O",6,1),P("P",2,3),Q("Q",1,8),R("R",6,1),S("S",6,1),T("T",6,1),U("U",6,1),V("V",2,4),W("W",1,10),X("X",1,10),Y("Y",1,10),Z("Z",1,10),JOKER(" ",2,0);
 	
 	private String affichageLettre;
-	private int recurrence;
-	private int point;
+	private final int recurrence;
+	private final int point;
 	
 	private ValeurLettre(String affichageLettre,int recurrence,int point) {
 		this.affichageLettre=affichageLettre;
@@ -14,5 +14,11 @@ public enum ValeurLettre {
 	}
 	public String AffichageLettre() {
 		return this.affichageLettre;
+	}
+	public int getRecurrence() {
+		return recurrence;
+	}
+	public int getPoint() {
+		return point;
 	}
 }
