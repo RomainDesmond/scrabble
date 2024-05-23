495a0ad0543fbd2ed3612a48b19214a8f57d0527
Romain DESMOND <romain.desmond@etu.unilim.fr>
2024-05-23 14:08:40 +0200

    Ajout d'une méthode jouerLettre qui permet d'ajouter la position du mot dans une liste. Ajout de la méthode jouerMot qui permet de placer le mot, Ajout d'une méthode qui permet de demander un nombre,

--- src/main/java/scrabble/application/ScrabbleApplicationConsole.java
+++ src/main/java/scrabble/application/ScrabbleApplicationConsole.java
@@ -30,7 +30,9 @@ public class ScrabbleApplicationConsole {
 		Scanner testInput = new Scanner(System.in);
 		
 		plateau.afficherPlateau();
-		
+		plateau.ajouteTypeCase();
+		plateau.afficherPlateau();
+
 		System.out.println("le plateau est vide :"+plateau.sacDeLettreEstVide());
 		plateau.afficherSacDeLettre();
 		plateau.remplirSacDeLettre();
@@ -49,17 +51,26 @@ public class ScrabbleApplicationConsole {
 		switch(choix) {
 		case 1:
 			echanger(plateau,j1);
-
+			break;
 			//TODO
 		case 2:
+			break;
+
 			//TODO
 		case 3:
-			//TODO
+			
+			jouerMot(plateau,j1);
+			break;
+
 		case 4:
+
+			break;
 			//TODO
 		}
-		j1.afficherChevalet();
 		plateau.afficherSacDeLettre();
+		plateau.afficherPlateau();
+		j1.afficherChevalet();
+
 
 
 		}
@@ -88,10 +99,10 @@ public class ScrabbleApplicationConsole {
 				System.out.println("Position de la lettre à échanger :");
 				int choixPosition=inputNombreEchange.nextShort();
 				positionsEchanger[compteur]=choixPosition;}			
-				//ajouter une vérification entre 0 et 7 et pas de doublon
-			}
+
+		}
 			for (compteur=0;compteur<choix;compteur++) {
-				plateau.ajouterLettrePosition(j.supprimerLettre(positionsEchanger[compteur]-1));
+				plateau.ajouterLettrePremierePositionDisponible(j.supprimerLettre(positionsEchanger[compteur]-1));
 				
 				distribuerUneLettre(plateau,j);
 					
@@ -126,6 +137,88 @@ public class ScrabbleApplicationConsole {
 		}
 		
 		
+	}
+	
+	public static void jouerLettre(int[] listeNb) {
+		System.out.println("Donnez une lettre à jouer");
+		Scanner inputNombreEchange = new Scanner(System.in);
+		int choixPosition=inputNombreEchange.nextShort();
+		int positionsLettre;
+		positionsLettre=choixPosition;
+		for (int cpt=0;cpt<listeNb.length;cpt++) {
+			System.out.println(listeNb[cpt]);
+			if (listeNb[cpt]==0) {
+			
+			
+				if (listeNb[cpt]==positionsLettre) {
+					break;
+				}
+				else {
+					listeNb[cpt]=positionsLettre;
+
+					break;
+				}
+			}
+			
+		}
+		
+	}
+	public static void jouerMot(Jeu plateau,Joueur j) {
+		int[] positionDeLaPremiereLettre=new int[2];
+		int[] listeDeNombre= new int[7];  //à modifier
+		int nombreLettreAPlacer=0;
+		int choix;
+		
+		System.out.println("1:ajout en ligne");
+		System.out.println("2:ajout en colonne");
+		choix=demanderNombre();
+
+		
+		System.out.println("Donnez le nombre de lettre que vouz voulez placer : ");
+		nombreLettreAPlacer=demanderNombre();
+		
+		for (int compteur=0;compteur<nombreLettreAPlacer;compteur++) {
+			jouerLettre(listeDeNombre);
+		}
+		//for qui met les lettres dans l'ordre dans la liste
+		
+		//positionner le mot
+		System.out.println("Donnez la ligne de la première lettre du mot :");
+		positionDeLaPremiereLettre[0]=demanderNombre();
+		System.out.println("Donnez la colonne de la première lettre du mot :");
+		positionDeLaPremiereLettre[1]=demanderNombre();
+
+		for(int cpt=0;cpt<nombreLettreAPlacer;cpt++) {
+			System.out.println();
+			plateau.afficherPlateau();
+
+			//System.out.println("Liste des positions :"+listeDeNombre[cpt]+j.donnerLettre(listeDeNombre[cpt]));
+
+			if (choix == 1) {
+					System.out.println("Lettre : "+j.donnerLettre(listeDeNombre[cpt]-1));
+					plateau.placerLettreJoue(positionDeLaPremiereLettre[0], positionDeLaPremiereLettre[1]+cpt,j.donnerLettre(listeDeNombre[cpt]-1));
+			}
+			else {
+				
+					System.out.println("test"+choix);
+					plateau.placerLettreJoue(positionDeLaPremiereLettre[0]+cpt, positionDeLaPremiereLettre[1], j.donnerLettre(listeDeNombre[cpt]-1));
+
+
+
+		}
+		}
+		//supprimer le mot dans le jeu du joueur et appeler une fonction qui remplit le sac du joueur
+		
+		
+	}
+	
+	public static int demanderNombre() {
+		//donnez des instructions avant cette méthode
+		Scanner inputNombre = new Scanner(System.in);
+		int choixPosition=inputNombre.nextShort();
+		int entier;
+		entier=choixPosition;
+		return choixPosition;
 	}
 
 	
