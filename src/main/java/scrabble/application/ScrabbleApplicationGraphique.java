f2b127f3d8c9a6eeea96a8a994d6c77f313766ee
Romain DESMOND <romain.desmond@etu.unilim.fr>
2024-06-06 21:00:41 +0200

    Instanciation de Joueur,plateau

--- src/main/java/scrabble/application/ScrabbleApplicationGraphique.java
+++ src/main/java/scrabble/application/ScrabbleApplicationGraphique.java
@@ -1,32 +1,53 @@
 package scrabble.application;
 
+import java.util.Scanner;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import scrabble.model.Jeu;
+import scrabble.model.Joueur;
 
 public class ScrabbleApplicationGraphique extends Application {
 
 	@Override
 	public void start(Stage primaryStage)throws Exception {
+		Joueur j=new Joueur("J1");
+		Jeu plateau = new Jeu();
+		plateau.ajouteTypeCase();
+
 		
-		Parent root;
-		FXMLLoader loader = new FXMLLoader( );
-		loader.setLocation(getClass().getResource("ScrabbleAffichage.fxml"));
-		root=loader.load();
+		//Parent root;
+		//FXMLLoader loader = new FXMLLoader( );
+		//loader.setLocation(getClass().getResource("ScrabbleAffichage.fxml"));
 		
-		VBox root1 = new VBox();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("ScrabbleAffichage.fxml"));
+        Parent root = loader.load();
+
+		
+        ScrabbleApplicationGraphiqueController controller = loader.getController();
+        GridPane idGrilleScrabble = controller.getIdGrilleScrabble();
+        
+        
+		ScrabbleApplicationGraphiqueController.actualiserAffichage(plateau,idGrilleScrabble);
+
+
 		
 		Scene scene = new Scene (root,900,900);
 		primaryStage.setScene(scene);
-		primaryStage.show();
-		System.out.println("etset");
-		
+		primaryStage.show();	
+		System.out.println("fin");
+		ScrabbleApplicationGraphiqueController.menuChoixJeu(j, plateau,idGrilleScrabble);
+		//menuChoix(j,plateau);
+		//ScrabbleApplicationGraphiqueController.actualiserAffichage(plateau);
 	}
 
 	public static void main(String[] args) {
 		launch(args);
 	}
+
 }
